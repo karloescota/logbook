7f6a4e6de5179537d24f40217c68c82fe79fd48c
Karlo Escota <karlo.escota@gmail.com>
2016-09-21 17:14:34 +0800

    display total time per day

--- app/models/log.rb
+++ app/models/log.rb
@@ -2,4 +2,10 @@ class Log < ApplicationRecord
   scope :current_log, -> { find_by("date(time_in) = ?", Date.current) }
 
   belongs_to :user
+
+  def total_time
+    return unless time_out
+    diff = time_out - time_in
+    Time.at(diff).utc.strftime("%H:%M")
+  end
 end
