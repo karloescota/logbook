6de72a5ee2d960879ea067abc13c3c5a81fd988b
Karlo Escota <karlo.escota@gmail.com>
2016-09-29 19:13:05 +0800

    fix dirty js, still dirty. add sessions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,4 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!
-
   def index
     @log = current_user.current_log || Log.new
     @current_week_logs = current_user.current_week_logs
